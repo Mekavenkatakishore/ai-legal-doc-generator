a4d247a9c25a09c23b5b243692976774bb7f6810
Meka venkata kishore <142087072+Mekavenkatakishore@users.noreply.github.com>
2026-03-09 21:29:58 +0530

    updated app.py file by genrated docs place

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-<<<<<<< HEAD
+from fileinput import filename
 import os
+from annotated_types import doc
 from flask import Flask, render_template, request, send_file
 from groq import Groq
 from docx import Document
@@ -71,8 +72,13 @@ def index():
         for line in output_text.split("\n"):
             doc.add_paragraph(line)
 
-        filename = instruction.lower().replace(" ", "_") + ".docx"
+        #filename = instruction.lower().replace(" ", "_") + ".docx"
+        #doc.save(filename)
+
+        filename = "generated_docs/" + instruction.lower().replace(" ", "_") + ".docx"
         doc.save(filename)
+
+
 
         return send_file(filename, as_attachment=True)
 
@@ -84,42 +90,3 @@ def index():
 # ================================
 if __name__ == "__main__":
     app.run(debug=True)
-=======
-
-
-def chatbot_response(user_input):
-    user_input = user_input.lower()
-
-    
-    if "hello" in user_input or "hi" in user_input:
-        return "Hello! How can I help you?"
-    elif "timing" in user_input or "hours" in user_input:
-        return "We are open from 9 AM to 9 PM."
-    elif "menu" in user_input or "serve" in user_input or "coffee" in user_input or "food" in user_input or "drinks" in user_input:
-        return """
-We have a wide range of delicious items!
-- Coffees: Espresso, Latte, Cappuccino, Americano, Mocha
-- Teas: Green Tea, Black Tea, Herbal Infusions
-- Food: Fresh Croissants, Sandwiches, Muffins, and Cake of the Day.
-What are you in the mood for?
-"""
-    elif "price" in user_input or "cost" in user_input:
-        return "Our pricing starts from $10."
-    
-    elif "bye" in user_input:
-        return "Goodbye! Have a great day 😊"
-    else:
-        return "Sorry, I didn’t understand that. Can you rephrase?"
-
-
-print("Chatbot: Hi! I'm your assistant. Type 'bye' to exit.")
-
-while True:
-    user = input("You: ")
-    response = chatbot_response(user)
-    print("Chatbot:", response)
-    if "bye" in user.lower():
-        break
-
-
->>>>>>> 1ae87026edc47e65fabfb2d0edbcb9b7356c1da4
